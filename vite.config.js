645588aa4c3a89ca2d62f503bc2bb7001bd4bab6
hsuyunyun <5b1g0011@stust.edu.tw>
2025-05-15 11:04:35 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/js250508/', // 這裡是 GitHub repository 的名稱
+// vite.config.js
+export default {
+  base: '/js250508/', // 根據你的 GitHub repository 名稱設置
   plugins: [react()],
-})
+}
