e950b1c3350197936c3d2a7f64f807e6b309d84e
hsuyunyun <5b1g0011@stust.edu.tw>
2025-05-15 10:16:24 +0800

    123

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/js250508/', // ⚠️ 這裡用你的 repo 名稱
-  plugins: [react()],
+  base: '/js250508/', // 換成你的 repo 名稱
+  plugins: [react()]
 })
